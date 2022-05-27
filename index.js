90e04c8c981fe333070909980fc954ab3d6451b8
marcelookasaki <marcelo.okasaki@gmail.com>
2022-05-27 16:54:03 -0300

    Passando parâmetros para consulta

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+const usuarios = [
+  {
+    id: 1,
+    nome: 'Elisa',
+    email: 'ea@email.com',
+    idade: 59
+  },
+  {
+    id: 2,
+    nome: 'Mariana',
+    email: 'mari@email.com',
+    idade: 12
+  },
+  {
+    id: 3,
+    nome: 'Marcelo',
+    email: 'ma@email.com',
+    idade: 54
+  },
+  {
+    id: 4,
+    nome: 'Fernando',
+    email: 'fe@email.com',
+    idade: 12
+  },
+  {
+    id: 5,
+    nome: 'Giulia',
+    email: 'giu@email.com',
+    idade: 4
+  }
+]
+
 const typeDefs = gql`
+
     scalar Date
 
     type Produto {    
@@ -25,7 +59,9 @@ const typeDefs = gql`
         horaAtual: Date
         usuarioLogado: Usuario    
         produtoEmDestaque: Produto  
-        numerosMegaSena: [Int!]!                    
+        numerosMegaSena: [Int!]!      
+        usuarios: [Usuario!]!      
+        usuario(id: ID): Usuario                        
       }
     `
 
@@ -76,6 +112,14 @@ const resolvers = {
       return Array(6).fill(0)
         .map(n => parseInt(Math.random() * 60 + 1))
         .sort(crescente)
+    },
+    usuarios() {
+      return usuarios
+    },
+    usuario(_, { id }) {
+      const selecionados = usuarios
+        .filter(u => u.id == id)
+      return selecionados ? selecionados[0] : null
     }
   }
 }
