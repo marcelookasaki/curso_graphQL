cc8977eae485ef8a4675c6937b34f7bab5ce7a83
marcelookasaki <marcelo.okasaki@gmail.com>
2022-05-27 16:09:32 -0300

    Tipos básicos do GraphQL

---
+++ index.js
@@ -0,0 +1,31 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+      scalar Date
+      # Pontos de entrada da API
+      type Query {
+        ola: String
+        horaAtual: Date
+      }
+    `
+
+const resolvers = {
+  Query: {
+    ola() {
+      return 'Bom dia!'
+    },
+    horaAtual() {
+      return new Date
+    }
+
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Executando em ${url}`)
+})
