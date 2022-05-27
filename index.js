5d26ab50ffee09d0650310e34e84a047d6ce3cb2
marcelookasaki <marcelo.okasaki@gmail.com>
2022-05-27 16:26:24 -0300

    Criando o primeiro tipo

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const { ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
       scalar Date
+
+       type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+      }
       # Pontos de entrada da API
       type Query {
         ola: String
         horaAtual: Date
+        usuarioLogado: Usuario          
       }
     `
 
@@ -16,8 +26,17 @@ const resolvers = {
     },
     horaAtual() {
       return new Date
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: 'Marcelo',
+        email: 'marcelo.okasaki@email.com',
+        idade: 54,
+        salario: 100000.09,
+        vip: true
+      }
     }
-
   }
 }
 
